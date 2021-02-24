91c6d71ed6f4d1f81fa0a7e9d72ec473e6ce7c74
Kan <77993386+Kan014@users.noreply.github.com>
2021-02-24 05:59:21 -0500

    Ejemplo # 2
    Código de ejemplo, con el evento ready simple con discord,js v12

---
+++ Github Class - 2021/DiscordBot/Others/index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js'); // Requerimos la librería
+const client = new Discord.Client(); // Definimos client
+
+// Evento ready
+client.on('ready', () => {
+  console.log(`Estoy listo sesión iniciada como ${client.user.tag}`); // Esto será retornado por la consola
+  client.user.setActivity(`Github.com`, { type: "WATCHING" }); // setActivity será el estado de nuestro, lo que se debe mostrar en el estado, según lo establecido [Viendo Github.com]
+});
+
+// Evento message
+client.on('message', message => { // Definimos message o msg
+  
+  if (message.content === 'ping') { // Creamos una definición
+    message.reply('Pong :ping_pong:!!'); // Creamos una respuesta
+  } // Cerramos
+}); // Volvemos a cerrar
+
+client.login('token'); // Definimos client
+
+
+// Luego iniciar el bot con " node index.js "
+// Estaré dejando una explicación sobre esto :)
